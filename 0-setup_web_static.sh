8027b3964c4a830e47490d107bdd74feb54333ff
Zelalem Abebe <zola2311@gmail.com>
2022-09-30 01:11:40 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 # Script that configures Nginx server with some folders and files
+
+# Install Nginx if it not already installed
 apt-get -y update
 apt-get -y install nginx
 service nginx start
+
+# Create the folders
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file
 echo "<html>
   <head>
   </head>
@@ -12,9 +18,15 @@ echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
 ln -fs /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the folder
 chown -R ubuntu /data/
 chown -R :ubuntu /data/
+
+# Update the Nginx configuration to server
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
