143592471ea455372a7d504ed07e9f41160dd3a0
Zelalem Abebe <zola2311@gmail.com>
2022-09-30 18:38:51 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""pack and deploy content to server
 """
-from fabric.api import local, env, run, put
-import os
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
+
+from fabric.api import put, run, env
+from os.path import exists
 
 env.hosts = ['44.192.52.135', '44.192.246.111']
 env.user = 'ubuntu'
@@ -22,7 +25,6 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        print("New version deployed!")
         return True
     except:
         return False
