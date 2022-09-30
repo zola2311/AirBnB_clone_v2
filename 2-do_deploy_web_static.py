88890cc664da7c5397ea043e6c400481ac0113e0
Zelalem Abebe <zola2311@gmail.com>
2022-09-30 18:41:55 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,29 +4,49 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import put, run, env
-from os.path import exists
+import os
+from fabric.api import *
+
 
 env.hosts = ['44.192.52.135', '44.192.246.111']
 env.user = 'ubuntu'
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    '''
+        Deploys an archive to the web servers
+    '''
+    name = archive_path.split("/")[1]
+    if not os.path.exists(archive_path):
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
+
+    result = put(archive_path, "/tmp/")
+    if result.failed:
         return False
+
+    run("mkdir -p /data/web_static/releases/{}".format(name[:-4]))
+
+    cmd = "tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(name,
+                                                                    name[:-4])
+    result = run(cmd)
+    if result.failed:
+        return False
+
+    result = run("rm /tmp/{}".format(name))
+    if result.failed:
+        return False
+
+    run("cp -rp /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}/".format(name[:-4], name[:-4]))
+
+    run("rm -rf /data/web_static/releases/{}/web_static/".format(name[:-4]))
+    result = run("rm /data/web_static/current")
+    if result.failed:
+        return False
+
+    path = "/data/web_static/releases/{}".format(name[:-4])
+    cmd = "ln -sf {} /data/web_static/current".format(path)
+    result = run(cmd)
+    if result.failed:
+        return False
+    return True
        
    
