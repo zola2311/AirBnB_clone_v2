76f733489dcbd96c5f3da8415372005c8ae62dbb
Zelalem Abebe <zola2311@gmail.com>
2022-10-16 16:30:24 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from os import getenv
-from sqlalchemy.sql.schema import ForeignKey
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        @property
-        def cities(self):
-            """"""""
-            list_c = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    list_c.append(city)
-            return list_c
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
